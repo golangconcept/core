f2b101607f3af024556e83d0bad65cdbe2086f5d
jpsi-0895 <jaipal.saini.0895@gmail.com>
2024-11-07 16:10:34 +0530

    I chose the interface name Appliance because if I were to draw a Venn diagram of a Laptop and a Washing machine the intersection would be that each piece of equipment has a power on switch

--- interface/interface.go
+++ interface/interface.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Printer interface {
 	Write()
-	Read()
+	// Read()
 }
 
 type Moniter struct {
@@ -14,8 +17,35 @@ func (m Moniter) Write() {
 	fmt.Println("Monioter hello")
 }
 
+func Print(p Printer) {
+	fmt.Println("seocond Monioter hello")
+
+}
+
+type Appliance interface {
+	On() error
+	Off() error
+}
+
+type Laptop struct {
+	power bool
+}
+
+func (l Laptop) On() error {
+
+	if !l.power {
+		return errors.New("need power")
+	}
+	return nil
+}
+
+func (l Laptop) Off() error {
+	return nil
+}
 func main() {
 
 	m := Moniter{}
 	m.Write()
+
+	Print(m)
 }
--- interface/interface.md
+++ interface/interface.md
@@ -7,3 +7,7 @@ An interface type in go kind of like a definition. It defines and describes the 
 One of the key features of Go is its support for interfaces. `Interfaces` in Go allow you to define a set of method signatures that any type can implement, providing a flexible and powerful way to write `generic code`.
 
 ## What are Go Interfaces?
+
+##  Interface Embedding in Go
+
+In addition to defining standalone interfaces, Go also allows you to embed interfaces within other interfaces. This is called `interface embedding`, and it provides a powerful way to compose interfaces.
